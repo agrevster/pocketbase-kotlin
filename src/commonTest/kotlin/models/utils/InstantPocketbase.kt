b8a346b286a1d51d9abf432ea75d0c8c9f07bcc7
sergeypdev <sergeypoznyak@protonmail.com>
2023-10-24 22:32:38 +0400

    Add InstantPocketbase type with custom Serializable

---
+++ src/commonTest/kotlin/models/utils/InstantPocketbase.kt
@@ -0,0 +1,35 @@
+package models.utils
+
+import TestingUtils
+import io.github.agrevster.pocketbaseKotlin.models.utils.InstantPocketbase
+import io.github.agrevster.pocketbaseKotlin.models.utils.InstantPocketbaseSerializer
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.contextual
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class InstantPocketbaseTest : TestingUtils() {
+    @Test
+    fun testSerializeDeserialize(): Unit {
+        @Serializable
+        data class TestSerializable(val instant: InstantPocketbase)
+
+        val original = TestSerializable(Instant.parse("2023-10-24T01:02:03.123Z"))
+
+        val encoded = Json.encodeToString(original)
+        assertEquals(
+            expected = "{\"instant\":\"2023-10-24 01:02:03.123Z\"}",
+            actual = encoded,
+        )
+        val decoded: TestSerializable = Json.decodeFromString(encoded)
+        assertEquals(
+            expected = original,
+            actual = decoded,
+        )
+    }
+}
