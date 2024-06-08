2307f0db33eb0709849939e88c504ba398dc96c8
agrevster <138630000+agrevster@users.noreply.github.com>
2024-06-08 17:46:11 -0500

    Upgraded to Kotlin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
-import org.jetbrains.kotlin.gradle.plugin.KotlinTargetPreset
 
 plugins {
-    val kotlinVersion = "1.8.22"
-    val dokkaVersion = "1.8.20"
+    val kotlinVersion = "2.0.0"
+    val dokkaVersion = "1.9.20"
     kotlin("multiplatform") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
     id("org.jetbrains.dokka") version dokkaVersion
@@ -11,17 +12,12 @@ plugins {
     id("signing")
 
 }
+
 val ktorVersion = "2.2.4"
 val kotlinSerializationVersion = "1.5.1"
 val kotlinCoroutinesVersion = "1.6.4"
 val kotlinTimeVersion = "0.4.0"
 
-val nativeCIOMainSets: MutableList<KotlinSourceSet> = mutableListOf()
-val nativeCIOTestSets: MutableList<KotlinSourceSet> = mutableListOf()
-val nativeWinHTTPMainSets: MutableList<KotlinSourceSet> = mutableListOf()
-val nativeWinHTTPTestSets: MutableList<KotlinSourceSet> = mutableListOf()
-
-val host: Host = getHostType()
 
 repositories {
     mavenCentral()
@@ -29,136 +25,126 @@ repositories {
 
 
 kotlin {
-    fun isPublishedBuild(): Boolean = System.getenv("PUBLISHING") == "true"
-
-    fun addNativeTarget(preset: KotlinTargetPreset<*>, desiredHost: Host) {
-        val target = targetFromPreset(preset)
-        if (desiredHost == Host.WINDOWS) {
-            nativeWinHTTPMainSets.add(target.compilations.getByName("main").kotlinSourceSets.first())
-            nativeWinHTTPTestSets.add(target.compilations.getByName("test").kotlinSourceSets.first())
-        } else {
-            nativeCIOMainSets.add(target.compilations.getByName("main").kotlinSourceSets.first())
-            nativeCIOTestSets.add(target.compilations.getByName("test").kotlinSourceSets.first())
-        }
-        if (host != desiredHost) {
-            target.compilations.configureEach {
-                compileKotlinTask.enabled = isPublishedBuild()
-            }
-        }
-    }
     explicitApi()
 
     jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
+        @OptIn(ExperimentalKotlinGradlePluginApi::class) compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_1_8)
         }
         withJava()
     }
 
-
     // Linux
-    addNativeTarget(presets["linuxX64"], Host.LINUX)
+    linuxX64()
 
     // MacOS
-    addNativeTarget(presets["macosX64"], Host.MAC_OS)
-    addNativeTarget(presets["macosArm64"], Host.MAC_OS)
+    macosX64()
+    macosArm64()
 
     // iOS
-    addNativeTarget(presets["iosArm64"], Host.MAC_OS)
-    addNativeTarget(presets["iosX64"], Host.MAC_OS)
-    addNativeTarget(presets["iosSimulatorArm64"], Host.MAC_OS)
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
 
     // Windows
-    addNativeTarget(presets["mingwX64"], Host.WINDOWS)
+    mingwX64()
 
     sourceSets {
-        val commonMain by getting {
+        commonMain {
             dependencies {
                 api("io.ktor:ktor-client-core:$ktorVersion")
                 api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
                 api("org.jetbrains.kotlinx:kotlinx-datetime:$kotlinTimeVersion")
                 api("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinSerializationVersion")
-
                 api("io.ktor:ktor-client-content-negotiation:$ktorVersion")
                 api("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
             }
         }
 
-        val commonTest by getting {
+        commonTest {
             dependencies {
                 implementation(kotlin("test"))
-
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:$kotlinTimeVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-
                 implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinSerializationVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$kotlinSerializationVersion")
             }
         }
 
-        val nativeCIOMain by creating {
-            dependsOn(commonMain)
+        val cioMain by creating {
+            dependsOn(commonMain.get())
+            dependencies {
+                api("io.ktor:ktor-client-cio:$ktorVersion")
+            }
+        }
+
+        val cioTest by creating {
+            dependsOn(commonTest.get())
             dependencies {
                 implementation("io.ktor:ktor-client-cio:$ktorVersion")
             }
         }
 
-        val nativeCIOTest by creating {
-            dependsOn(commonTest)
+        val winHTTPMain by creating {
+            dependsOn(commonMain.get())
             dependencies {
-                implementation("io.ktor:ktor-client-cio:$ktorVersion")
+                api("io.ktor:ktor-client-winhttp:$ktorVersion")
             }
         }
 
-        val nativeWinHTTPMain by creating {
-            dependsOn(commonMain)
-            dependencies {
-                implementation("io.ktor:ktor-client-winhttp:$ktorVersion")
-            }
-        }
-
-        val nativeWinHTTPTest by creating {
-            dependsOn(commonTest)
+        val winHTTPTest by creating {
+            dependsOn(commonTest.get())
             dependencies {
                 implementation("io.ktor:ktor-client-winhttp:$ktorVersion")
             }
         }
 
-        val jvmMain by getting { dependsOn(nativeCIOMain) }
-        val jvmTest by getting { dependsOn(nativeCIOTest) }
+        fun KotlinSourceSet.configureDependencies(
+            test: Boolean = false,
+            winHTTP: Boolean = false,
+        ) {
+            if (winHTTP) {
+                if (test) {
+                    this.dependsOn(winHTTPTest)
+                } else
+                    this.dependsOn(winHTTPMain)
+            }else
+                if (test) {
+                    this.dependsOn(cioTest)
+                } else
+                    this.dependsOn(cioMain)
+        }
 
-        configure(nativeCIOMainSets) { dependencies { dependsOn(nativeCIOMain) } }
-        configure(nativeCIOTestSets) { dependencies { dependsOn(nativeCIOTest) } }
+        getByName("jvmMain").configureDependencies()
+        getByName("jvmTest").configureDependencies(test = true)
 
-        configure(nativeWinHTTPMainSets) { dependencies { dependsOn(nativeWinHTTPMain) } }
-        configure(nativeWinHTTPTestSets) { dependencies { dependsOn(nativeWinHTTPTest) } }
+        getByName("linuxX64Main").configureDependencies()
+        getByName("linuxX64Test").configureDependencies(test = true)
+
+        getByName("macosX64Main").configureDependencies()
+        getByName("macosX64Test").configureDependencies(test = true)
+
+        getByName("macosArm64Main").configureDependencies()
+        getByName("macosArm64Test").configureDependencies(test = true)
+
+        getByName("iosArm64Main").configureDependencies()
+        getByName("iosArm64Test").configureDependencies(test = true)
+
+        getByName("iosSimulatorArm64Main").configureDependencies()
+        getByName("iosSimulatorArm64Test").configureDependencies(test = true)
+
+        getByName("iosX64Main").configureDependencies()
+        getByName("iosX64Test").configureDependencies(test = true)
+
+        getByName("macosArm64Main").configureDependencies()
+        getByName("macosArm64Test").configureDependencies(test = true)
+
+        getByName("mingwX64Main").configureDependencies(winHTTP = true)
+        getByName("mingwX64Test").configureDependencies(winHTTP = true, test = true)
     }
 }
-
-fun getHostType(): Host {
-    val hostOs = System.getProperty("os.name")
-    return when {
-        hostOs.startsWith("Windows") -> Host.WINDOWS
-        hostOs.startsWith("Mac") -> Host.MAC_OS
-        hostOs == "Linux" -> Host.LINUX
-        else -> throw Error("Invalid host.")
-    }
-}
-
-fun isPublicationAllowed(name: String): Boolean {
-    println("name: $name")
-    return when {
-        name.startsWith("mingw") -> host == Host.WINDOWS
-        name.startsWith("macos") ||
-                name.startsWith("ios") ||
-                name.startsWith("watchos") ||
-                name.startsWith("tvos") -> host == Host.MAC_OS
-        else -> host == Host.LINUX
-    }
-}
-enum class Host { WINDOWS, MAC_OS, LINUX }
 
 apply(from = "publish.gradle")
